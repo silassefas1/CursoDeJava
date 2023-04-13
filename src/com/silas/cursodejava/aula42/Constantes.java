adb57dad303158977d0059640e49d53863c0952e
Silas Sefas <103271018+silassefas1@users.noreply.github.com>
2023-04-13 08:29:24 -0300

    aula 43
    classe object , a classe pai de todas no java

--- src/com/silas/cursodejava/aula42/Constantes.java
+++ src/com/silas/cursodejava/aula42/Constantes.java
@@ -3,6 +3,6 @@ package com.silas.cursodejava.aula42;
 public final class Constantes {
 	
 	public static final String MINHA_CONSTANTE = "https://loiane.com";
-	public static  String CURSO_COMPLETO = "https://https://loiane.training/continuar-curso/java-basico";
+	public static final String CURSO_COMPLETO = "https://https://loiane.training/continuar-curso/java-basico";
 
 }
---
+++ src/com/silas/cursodejava/aula43/teste.java
@@ -0,0 +1,5 @@
+package com.silas.cursodejava.aula43;
+
+public class teste {
+
+}
